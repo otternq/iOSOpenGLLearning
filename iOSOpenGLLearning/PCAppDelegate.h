0c742d54b8dfb938235de261487a5a4228c4692d
Nick Otter <otternq@gmail.com>
2012-11-15 13:46:10 -0800

    Initial Commit

---
+++ iOSOpenGLLearning/PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  iOSOpenGLLearning
+//
+//  Created by Nicholas Otter on 11/15/12.
+//  Copyright (c) 2012 otternq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSOpenGLLearning/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSOpenGLLearning
+//
+//  Created by Nicholas Otter on 11/15/12.
+//  Copyright (c) 2012 otternq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCAppDelegate class]));
+    }
+}
